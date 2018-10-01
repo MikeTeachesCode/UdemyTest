756f7de903476a19106cb3d5095d59ccf37f1fdf
MichaelQuant4R <Michael.R2014@yahoo.co.uk>
2018-10-01 08:55:10 +0100

    initial commit

---
+++ gui1.py
@@ -0,0 +1,13 @@
+import tkinter as tk
+
+gui = tk.Tk()
+
+gui.geometry("300x400")
+
+ypos = [0, 40, 80]
+
+for i in range(len(ypos)):
+    lab1 = tk.Label(gui, text = "This is a test!")
+    lab1.place(x=20, y = 40+ypos[i])
+
+gui.mainloop()
